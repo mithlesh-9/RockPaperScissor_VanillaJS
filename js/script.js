2b93db0c2c9c3df1535023388d0ec41dd04741c4
mithleshfreak <mithlexh@gmail.com>
2021-07-18 17:41:24 +0545

    Code Refactor

--- js/script.js
+++ js/script.js
@@ -1,33 +1,36 @@
 let scoreElem = document.querySelector('.digit');
+let HANDSTYPE = ['rock', 'paper', 'scissor'];
+
+let isWinner = {
+	//rock
+	[HANDSTYPE[0]]: {
+		[HANDSTYPE[1]]: false,
+		[HANDSTYPE[2]]: true,
+	},
+	//paper
+	[HANDSTYPE[1]]: {
+		[HANDSTYPE[0]]: true,
+		[HANDSTYPE[1]]: false,
+	},
+	//scissor
+	[HANDSTYPE[2]]: {
+		[HANDSTYPE[1]]: true,
+		[HANDSTYPE[0]]: false,
+	},
+};
 
 function chooseSymbol(handSelected) {
 	window.localStorage.setItem('user', handSelected);
 	gamePlay(handSelected);
-	// e.target.style = 'transform: translate(10px, 10px)';
 }
 
-function gamePlay(user) {
-	document.querySelector('.choices') &&
-		document.querySelector('.choices').remove();
-	let paper = createHand('rock');
-	let scissor = createHand('paper');
-	let rock = createHand('scissor');
-
-	let alignment = document.createElement('div');
-	alignment.classList.add('alignment');
-
-	let handWrapper = document.createElement('div');
-	handWrapper.classList.add('hand-wrapper');
-	handWrapper.style.order = '-1';
-	let txt = document.createElement('div');
-	txt.classList.add('txt');
-	txt.textContent = 'You Picked';
-
-	handWrapper.append(txt);
-	handWrapper.append(rock);
-
-	alignment.append(handWrapper);
-
+function computerChoice(
+	user,
+	userElement,
+	houseLoading,
+	alignment,
+	houseHandWrapper
+) {
 	setTimeout(() => {
 		let paper = createHand('rock');
 		let scissor = createHand('paper');
@@ -35,10 +38,31 @@ function gamePlay(user) {
 
 		let info = document.createElement('div');
 		info.classList.add('info');
-		info.style.order = '0';
+		// info.style.order = '0';
 		let infoTxt = document.createElement('div');
 		infoTxt.classList.add('text');
-		infoTxt.textContent = 'You Won!';
+
+		let house = HANDSTYPE[Math.floor(Math.random() * 3)];
+		let houseElement =
+			house === 'rock' ? rock : house === 'paper' ? paper : scissor;
+
+		let newScore = window.localStorage.getItem('score');
+
+		if (isWinner[user][house]) {
+			newScore++;
+			infoTxt.textContent = 'You Won';
+			userElement.classList.add('winner');
+		}
+
+		if (isWinner[user][house] == false) {
+			newScore--;
+			infoTxt.textContent = 'You Lose';
+			houseElement.classList.add('winner');
+		}
+
+		if (isWinner[user][house] == undefined) {
+			infoTxt.textContent = 'Draw';
+		}
 
 		let playAgain = document.createElement('button');
 		playAgain.classList.add('btn');
@@ -55,12 +79,38 @@ function gamePlay(user) {
 
 		alignment.append(info);
 		houseLoading.remove();
-		houseHandWrapper.append(rock);
-		let newScore = window.localStorage.getItem('score');
-		newScore++;
+
+		houseHandWrapper.append(houseElement);
+
 		window.localStorage.setItem('score', newScore);
 		scoreElem.textContent = newScore;
 	}, 2000);
+}
+
+function gamePlay(user) {
+	document.querySelector('.choices') &&
+		document.querySelector('.choices').remove();
+	let paper = createHand('rock');
+	let scissor = createHand('paper');
+	let rock = createHand('scissor');
+
+	let userElement = user === 'rock' ? rock : user === 'paper' ? paper : scissor;
+
+	let alignment = document.createElement('div');
+	alignment.classList.add('alignment');
+
+	let handWrapper = document.createElement('div');
+	handWrapper.classList.add('hand-wrapper');
+	handWrapper.style.order = '-1';
+	let txt = document.createElement('div');
+	txt.classList.add('txt');
+	txt.textContent = 'You Picked';
+
+	handWrapper.append(txt);
+
+	handWrapper.append(userElement);
+
+	alignment.append(handWrapper);
 
 	let houseHandWrapper = document.createElement('div');
 	houseHandWrapper.classList.add('hand-wrapper');
@@ -78,21 +128,23 @@ function gamePlay(user) {
 
 	alignment.append(houseHandWrapper);
 
+	computerChoice(user, userElement, houseLoading, alignment, houseHandWrapper);
+
 	document.querySelector('body').append(alignment);
 }
 
 function showHands() {
 	let choices = document.createElement('div');
 	choices.classList.add('choices');
-	// choices.style =
-	// 	"background:url('./images/bg-triangle.svg');background-repeat: no-repeat;background-size:70%;background-position: 62px 70px";
-	let paper = createHand('rock');
-	let scissor = createHand('paper');
-	let rock = createHand('scissor');
+	choices.style =
+		"background:url('./images/bg-triangle.svg');background-repeat: no-repeat;background-size:70%;background-position: 62px 70px";
+	let paper = createHand('rock', 'has-hover');
+	let scissor = createHand('paper', 'has-hover');
+	let rock = createHand('scissor', 'has-hover');
 
-	scissor.addEventListener('click', () => chooseSymbol('scissor'));
-	rock.addEventListener('click', () => chooseSymbol('rock'));
-	paper.addEventListener('click', () => chooseSymbol('paper'));
+	scissor.addEventListener('click', () => chooseSymbol('scissor', scissor));
+	rock.addEventListener('click', () => chooseSymbol('rock', rock));
+	paper.addEventListener('click', () => chooseSymbol('paper', paper));
 
 	choices.append(rock);
 	choices.append(paper);
@@ -144,10 +196,14 @@ ruleBtn.addEventListener('click', function (e) {
 	showRules();
 });
 
-function createHand(symbol) {
+function createHand(symbol, className) {
 	let hand = document.createElement('div');
 	hand.classList.add('outer-circle');
 	hand.classList.add(symbol);
+
+	if (className) {
+		hand.classList.add(className);
+	}
 
 	hand.innerHTML = `<div class="inner-circle">
 	           <img src="./images/icon-${symbol}.svg" ></div>`;
